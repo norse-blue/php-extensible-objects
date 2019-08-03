165c74ad3c86e9e4bd2af27ea705c83b2799c8a5
Axel Pardemann <axel.pardemann@norse.blue>
2019-08-03 15:27:52 -0500

    :art: Declare ExtensibleObject as abstract
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- src/ExtensibleObject.php
+++ src/ExtensibleObject.php
@@ -7,7 +7,7 @@ namespace NorseBlue\ExtensibleObjects;
 use NorseBlue\ExtensibleObjects\Contracts\Extensible;
 use NorseBlue\ExtensibleObjects\Traits\HandlesExtensionMethods;
 
-class ExtensibleObject implements Extensible
+abstract class ExtensibleObject implements Extensible
 {
     use HandlesExtensionMethods;
 }
